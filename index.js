3bc3e177105f70081ad67955f98995a1e83ef36e
Shakhzod <forworkingappsinmylaptop@gmail.com>
2022-03-19 10:05:07 +0500

    learnt JS today

---
+++ index.js
@@ -0,0 +1,9 @@
+//  Array - massive, to'plam = malumotlarni toplash maqsadida ishlatiladi.
+// var ourArray = [23, "sjjsj", 3839, "**&*&*)_)", 3, null, undefined, "test"];
+
+const fruits = ["Banana", "Orange", "Apple", "Mango", 23];
+var veggies = ["carrots", "onions", "brocoli"];
+console.log(fruits.concat(veggies));
+// "Apple"
+// Boolean  true=rost, false=yog'ol,
+// .indexOf() ..includes() .concat()
